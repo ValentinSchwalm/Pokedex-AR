2783f42f841c71d8e2423a9962e8f63e6346e080
Strearm <92020515+Strearm@users.noreply.github.com>
2023-11-19 22:37:40 +0100

    text is now able to change

--- PokedexAR/Assets/ImageRecognitionExample.cs
+++ PokedexAR/Assets/ImageRecognitionExample.cs
@@ -1,49 +1,124 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ImageRecognitionExample : MonoBehaviour
 {
-    private ARTrackedImageManager _arTrackedImageManager;
-    [SerializeField] private TextMeshProUGUI infoText;
+    private ARTrackedImageManager m_ImageManager;
 
-    //private GameObject[] placedObjects;
-    //private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
+    [SerializeField]
+    private GameObject welcomePanel;
 
-    private void Awake()
+    [SerializeField]
+    private TextMeshProUGUI imageTrackedText;
+
+    [SerializeField]
+    private GameObject[] arObjectsToPlace;
+
+    [SerializeField]
+    private Vector3 scaleFactor = new Vector3(0.1f, 0.1f, 0.1f);
+
+    private ARTrackedImageManager m_TrackedImageManager;
+
+    private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
+
+    String currentActiveQR;
+
+
+    public void Awake()
     {
-        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
-        //foreach(GameObject gameObject in placedObjects)
-        //{
-        //    GameObject newArObject = Instantiate(gameObject);
-        //    newArObject.name = gameObject.name;
-        //    arObjects.Add(gameObject.name, newArObject);
-        //}
-    }
+        //dismissButton.onClick.AddListener(Dismiss);
+        m_ImageManager = GetComponent<ARTrackedImageManager>();
 
-    public void OnEnable()
-    {
-        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
-    }
-
-    public void OnDisable()
-    {
-        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
-    }
-
-    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
-    {
-        foreach (var trackedImage in args.added)
+        foreach (GameObject arObject in arObjectsToPlace)
         {
-            Debug.Log(trackedImage.name);
-            this.infoText.text = trackedImage.name;
+            GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
+            newARObject.name = arObject.name;
+            arObjects.Add(arObject.name, newARObject);
         }
-        //foreach(ARTrackedImage trackedImage in args.removed)
-        //{
-        //    arObjects[trackedImage.name].SetActive(false);
-        //}
     }
+
+
+    void OnEnable()
+    {
+        m_ImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_ImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            currentActiveQR = trackedImage.referenceImage.name;
+            UpdateARImage(trackedImage, currentActiveQR);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            if (trackedImage.trackingState == TrackingState.Tracking)
+            {
+                currentActiveQR = trackedImage.referenceImage.name;
+                imageTrackedText.text = currentActiveQR;
+                AssignGameObject(currentActiveQR, trackedImage.transform.position);
+            }
+            else
+            {
+                if (currentActiveQR != trackedImage.referenceImage.name)
+                {
+                    ReAssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position);
+                }
+            }
+        }
+    }
+    private void UpdateARImage(ARTrackedImage trackedImage, String name)
+    {
+        imageTrackedText.text = trackedImage.referenceImage.name;
+        AssignGameObject(name, trackedImage.transform.position);
+
+    }
+
+    void AssignGameObject(string name, Vector3 newPosition)
+    {
+        if (arObjectsToPlace != null)
+        {
+            GameObject goARObject = arObjects[name];
+            goARObject.SetActive(true);
+            goARObject.transform.position = newPosition;
+            goARObject.transform.localScale = scaleFactor;
+            foreach (GameObject go in arObjects.Values)
+            {
+                //Debug.Log($"Go in arObjects.Values: {go.name}");
+                if (go.name != name)
+                {
+                    go.SetActive(false);
+                }
+            }
+        }
+    }
+    void ReAssignGameObject(string name, Vector3 newPosition)
+    {
+        if (arObjectsToPlace != null)
+        {
+            GameObject goARObject = arObjects[name];
+            goARObject.SetActive(true);
+            goARObject.transform.position = newPosition;
+            goARObject.transform.localScale = scaleFactor;
+            foreach (GameObject go in arObjects.Values)
+            {
+                if (go.name == name)
+                {
+                    go.SetActive(false);
+                }
+            }
+        }
+    }
+    private void Dismiss() => welcomePanel.SetActive(false);
 }
