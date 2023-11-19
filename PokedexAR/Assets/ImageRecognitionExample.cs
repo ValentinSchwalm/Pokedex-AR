e6b54d6a04d6338646f45b5882276fd6b6c2b806
schwalmentin <schwalm@gmx.at>
2023-11-19 23:42:40 +0100

    Playersettings updated

--- PokedexAR/Assets/ImageRecognitionExample.cs
+++ PokedexAR/Assets/ImageRecognitionExample.cs
@@ -1,59 +1,35 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class ImageRecognitionExample : MonoBehaviour
 {
-    private ARTrackedImageManager m_ImageManager;
+    private ARTrackedImageManager imageManager;
 
-    [SerializeField]
-    private GameObject welcomePanel;
-
-    [SerializeField]
-    private TextMeshProUGUI imageTrackedText;
-
-    [SerializeField]
-    private GameObject[] arObjectsToPlace;
-
-    [SerializeField]
-    private Vector3 scaleFactor = new Vector3(0.1f, 0.1f, 0.1f);
-
-    private ARTrackedImageManager m_TrackedImageManager;
-
-    private Dictionary<string, GameObject> arObjects = new Dictionary<string, GameObject>();
+    [SerializeField] private TextMeshProUGUI imageTrackedText;
 
     String currentActiveQR;
 
 
-    public void Awake()
+    private void Awake()
     {
-        //dismissButton.onClick.AddListener(Dismiss);
-        m_ImageManager = GetComponent<ARTrackedImageManager>();
-
-        foreach (GameObject arObject in arObjectsToPlace)
-        {
-            GameObject newARObject = Instantiate(arObject, Vector3.zero, Quaternion.identity);
-            newARObject.name = arObject.name;
-            arObjects.Add(arObject.name, newARObject);
-        }
+        imageManager = GetComponent<ARTrackedImageManager>();
     }
 
-
-    void OnEnable()
+    private void OnEnable()
     {
-        m_ImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        imageManager.trackedImagesChanged += OnTrackedImagesChanged;
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
-        m_ImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+        imageManager.trackedImagesChanged -= OnTrackedImagesChanged;
     }
-    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
